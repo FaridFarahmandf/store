b3a40d03568f4b981d5bcf4771a0c0451902ce80
FaridFarahmand <faridfarahmandf2f@gmail.com>
2021-03-08 13:55:10 +0330

    adding range value to Product

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -5,20 +5,33 @@ import './productList.css'
 export default function ProductList() {
     const productState = useSelector(state => state.product)
     const [searchTerm , setSearchTerm] = useState('')
- 
+    const [range , setRange] = useState(0)
+    
+    function rangeValueHandler (e) {
+        setRange(e.target.value)
+    }
     return (
         <section>
-            <div className='d-flex flex-wrap'>
-                <div className='search'>
+            <div>
+                <div className='search d-flex flex-wrap'>
                     
                     <label>Filter by Brand Name : </label><input className='searchIn' type='text' placeholder='Search ...' 
                     onChange={event => setSearchTerm(event.target.value)} />
+
+                <div class="slidecontainer ms-4">
+                <input type="range" min="600" max="850" value={range} onChange={rangeValueHandler} class="slider" id="myRange"/>
+                <p>Price Range: <span id="demo">{range}</span></p>
                 </div>
-                <div className='d-flex flex-wrap'>
+                </div>
+                <div className='d-flex flex-wrap '>
                     {productState.filter(p => {
-                        if(searchTerm === '') {
+                        if(searchTerm === '' && range === 0) {
                             return  <Product key={p.id} {...p}/>
-                        }else if(p.brand.toLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+                        }else if(p.brand.toLowerCase().includes(searchTerm.toLocaleLowerCase()) && 
+                                range < p.price) {
+                            return <Product key={p.id} {...p}/>
+                        }else if(p.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()) &&
+                                range < p.price) {
                             return <Product key={p.id} {...p}/>
                         }
                     }).map((p) => <Product key={p.id} {...p}/>)}
